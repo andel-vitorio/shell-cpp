818c921a2af2269437f77e0fbfe44a6bc082e681
Andevaldo Vitório <andevaldo.vitorio@icomp.ufam.edu.br>
2023-10-19 18:47:50 -0400

    feat(commands): add touch command

--- inc/Shell.hpp
+++ inc/Shell.hpp
@@ -17,52 +17,64 @@
 
 #include "Command.hpp"
 
+enum ShellStatus {
+  SUCCESS, FAILURE, OPEN_FILE_FAILURE, CLOSE_FILE_FAILURE
+};
+
 class Shell
 {
 
 private:
   Command<std::string, const std::string &> echo;
   Command<std::string> pwd;
+  Command<int, const std::string &> touch;
 
 public:
-
   bool setup()
   {
     bool status = false;
 
     echo.setName("echo")
-      .setDescription("Prints a message on the screen.")
-      .setAction(
-        [](const std::string& message) -> std::string
-        {
-          std::cout << message << '\n';
-          return message;
-        }
-      );
-
-    auto pwdAction = [this]() -> std::string
-    {
-      const std::string& currentDir = get_current_dir_name();
-      this->echo.execute(currentDir);
-      return currentDir;
-    };
-
-    std::function<std::string()> pwdFunction = [pwdAction]() {
-        return pwdAction();
-    };
+        .setDescription("Prints a message on the screen.")
+        .setAction(
+          [](const std::string &message) -> std::string
+          {
+            std::cout << message << '\n';
+            return message;
+          });
 
     pwd.setName("pwd")
-      .setDescription("Gets the current directory.")
-      .setAction(pwdFunction);
+        .setDescription("Gets the current directory.")
+        .setAction(
+          []() -> std::string
+          {
+            const std::string &currentDir = get_current_dir_name();
+            std::cout << currentDir << '\n';
+            return currentDir;
+          });
+
+    touch.setName("touch")
+        .setDescription("Generates a blank file.")
+        .setAction(
+          [](const std::string &filename) -> int
+          {
+            mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+            int fd = open(filename.c_str(), O_WRONLY | O_CREAT, mode);
+
+            if (fd < 0)
+              return OPEN_FILE_FAILURE;
+
+            if (close(fd) < 0)
+              return CLOSE_FILE_FAILURE;
+
+            return SUCCESS;
+          });
 
     return true;
   }
 
-  int init()
-  {
-    this->echo.execute("Andel");
-    this->pwd.execute();
-
+  int init() {
+    
     return 0;
   }
 };
