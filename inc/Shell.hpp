41e3c65244b3411e9345ed7afbcb74c95fcdebae
Andevaldo Vitório <andevaldo.vitorio@icomp.ufam.edu.br>
2023-10-19 18:37:13 -0400

    feat(commands): add pwd command

--- inc/Shell.hpp
+++ inc/Shell.hpp
@@ -2,6 +2,18 @@
 #define __SHELL_HPP__
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <unistd.h>
+#include <dirent.h>
+#include <sstream>
+#include <regex>
+#include <iomanip>
+#include <map>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
 #include "Command.hpp"
 
@@ -10,6 +22,7 @@ class Shell
 
 private:
   Command<std::string, const std::string &> echo;
+  Command<std::string> pwd;
 
 public:
 
@@ -27,12 +40,28 @@ public:
         }
       );
 
+    auto pwdAction = [this]() -> std::string
+    {
+      const std::string& currentDir = get_current_dir_name();
+      this->echo.execute(currentDir);
+      return currentDir;
+    };
+
+    std::function<std::string()> pwdFunction = [pwdAction]() {
+        return pwdAction();
+    };
+
+    pwd.setName("pwd")
+      .setDescription("Gets the current directory.")
+      .setAction(pwdFunction);
+
     return true;
   }
 
   int init()
   {
     this->echo.execute("Andel");
+    this->pwd.execute();
 
     return 0;
   }
